195b3ac8d71057b77fbdd0308e700a5cc55d9d36
Tornike Giorgaia <tornike.giorgaia.1@btu.edu.ge>
2022-05-30 15:51:05 +0400

    ADD csv-to-sqlite Function

--- Parser.py
+++ Parser.py
@@ -1,4 +1,5 @@
 import csv
+import sqlite3
 import time
 from random import randint
 
@@ -11,7 +12,7 @@ def containsNumber(value):
 
 
 def scraping_steam():
-    page = int(input("From How Many pages do you want to get a Data:\n"))
+    page = int(input("From How Many pages do you want to get a Data:\nINPUT:"))
     with open("Top_Sellers.csv", "w", encoding="utf-8_sig", newline="\n") as file:
         file_obj = csv.writer(file)
         file_obj.writerow(
@@ -19,18 +20,18 @@ def scraping_steam():
                 "Id",
                 "Title",
                 "Release Date",
-                "Old Price ($)",
-                "Discount (%)",
-                "New Price ($)",
+                "Old Price",
+                "Discount",
+                "New Price",
                 "Platforms",
-                "Positive Ratings (%)",
+                "Positive Ratings",
                 "Reviews Count",
                 "IMAGE",
             ]
         )
 
         index = 1
-        for ind in range(1, page+1):
+        for ind in range(1, page + 1):
             url = f"https://store.steampowered.com/search/?category1=998&filter=topsellers&page={ind}"
 
             r = requests.get(url)
@@ -45,9 +46,13 @@ def scraping_steam():
                 platforms_soup = game.find_all("span", class_="platform_img")
 
                 try:
-                    ratings = game.find("span", class_="search_review_summary").attrs.get("data-tooltip-html")
+                    ratings = game.find(
+                        "span", class_="search_review_summary"
+                    ).attrs.get("data-tooltip-html")
                     ratings = ratings.replace("<br>", " ").split()
-                    positive_rating, users = tuple(el for el in ratings if containsNumber(el))
+                    positive_rating, users = tuple(
+                        el for el in ratings if containsNumber(el)
+                    )
                     positive_rating = positive_rating[:-1]
                     users = users.replace(",", "")
                 except AttributeError:
@@ -86,5 +91,46 @@ def scraping_steam():
         print("Done!")
 
 
+def csv_to_sqlite():
+    conn = sqlite3.connect("Top_Sellers.sqlite")
+    cur = conn.cursor()
+
+    # Dropping a Table
+    cur.execute(
+        """--sql
+        DROP TABLE if exists data;
+        """
+    )
+
+    create_table = """--sql
+                    CREATE TABLE IF NOT EXISTS data(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Title TEXT NOT NULL,
+                    Release_Date INTEGER NOT NULL,
+                    Old_Price TEXT,
+                    Discount INTEGER,
+                    New_Price TEXT,
+                    Platforms TEXT,
+                    Positive_Ratings INTEGER,
+                    Reviews_Count INTEGER,
+                    Images BLOB);
+                    """
+
+    cur.execute(create_table)
+
+    file = open("Top_Sellers.csv")
+
+    contents = csv.reader(file)
+    headings = next(contents)
+
+    insert_records = """INSERT INTO data (id, Title, Release_Date, Old_Price, Discount, New_Price, Platforms, Positive_ratings, Reviews_Count, Images) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+
+    cur.executemany(insert_records, contents)
+
+    conn.commit()
+    conn.close()
+
+
 if __name__ == "__main__":
     scraping_steam()
+    csv_to_sqlite()
